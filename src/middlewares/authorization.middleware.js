7641550b81ece5d2d2f821f99c54b09a8182fd73
Aizquel Urdaneta <aizquelus@gmail.com>
2025-02-14 22:21:09 -0300

    Agregando return temprano faltante en authorization middleware

--- src/middlewares/authorization.middleware.js
+++ src/middlewares/authorization.middleware.js
@@ -1,7 +1,7 @@
 export const authorization = (role) => {
     return async (req, res, next) => {
         if(!req.user) return res.status(401).json({ error: "No autorizado"});
-        if(req.user.role !== role) res.status(403).json({ error: "Rol inválido"});
+        if(req.user.role !== role) return res.status(403).json({ error: "Rol inválido"});
         next();
     }
 }
